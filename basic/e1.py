6a9953b510b398c9cea6a83760ff77a9e595ba6b
Gison <221387542+vigneshbytes@users.noreply.github.com>
2026-01-20 20:58:46 +0530

    refined p1, added doc

--- basic/e1.py
+++ basic/e1.py
@@ -1,6 +1,16 @@
+"""
+Given two integer numbers, write a Python program to return their product only if 
+the product is equal to or lower than 1000. Otherwise, return their sum.
+"""
+
+
 def multisum(x,y):
     return x*y if x*y <= 1000 else x+y
 
-a,b = input("enter two numbers: ").split()
+# a,b = input("enter two numbers: ").split()
 
-print(multisum(int(a), int(b)))
+a,b = map(int, input("enter two numbers: ").split())
+
+# print(multisum(int(a), int(b)))
+
+print(multisum(a, b))
