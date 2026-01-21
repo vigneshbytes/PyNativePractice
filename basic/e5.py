8bddd6d1a6ca0eee47ffa05b8f700fc05a837f37
Gison <221387542+vigneshbytes@users.noreply.github.com>
2026-01-21 17:19:21 +0530

    alternatie structure for e5.py

--- basic/e5.py
+++ basic/e5.py
@@ -1,12 +1,15 @@
 def first_last(num):
-    print("for the numbers {} equivalence of first and last is: {}".format(num, num[0] == num[-1]))
-
+#    print("for the numbers {} equivalence of first and last is: {}".format(num, num[0] == num[-1]))
+    return num[0] == num[-1]
 
 numbers_x = [10, 20, 30, 40, 10]
 numbers_y = [75, 65, 35, 75, 30]
 
-first_last(numbers_x)
-first_last(numbers_y)
+#first_last(numbers_x)
+#first_last(numbers_y)
+
+print("for the numbers {} equivalence of first and last is: {}".format(numbers_x, first_last(numbers_x)))
+print("for the numbers {} equivalence of first and last is: {}".format(numbers_y, first_last(numbers_y)))
 
 """
 Write a code to return True if the list’s first and last numbers are the same. If the numbers are different, return False.
